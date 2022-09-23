ca02efb664ecd71fdd344b54f3d71d9949264d75
Nassim555 <48863215+Nassim555@users.noreply.github.com>
2022-09-23 21:47:18 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 const btn = document.querySelector(".share-btn");
 const social = document.querySelector(".social");
-console.log(btn);
+
 btn.addEventListener("click", () => {
-  social.classList.toggle("show");
-  btn.classList.toggle("active");
+  if (social.classList.contains("show")) {
+    btn.classList.remove("active");
+    social.classList.remove("show");
+    social.classList.add("closing");
+    social.addEventListener(
+      "animationend",
+      () => {
+        social.classList.remove("closing");
+      },
+      { once: true },
+    );
+  } else {
+    btn.classList.add("active");
+    social.classList.add("show");
+  }
 });
